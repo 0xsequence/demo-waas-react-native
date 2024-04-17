a3608d11086f612033a6efa0fec7d09b5a417e8e
Tolgahan Arikan <tolgahan.arikan@gmail.com>
2024-04-17 13:34:36 +0300

    wip

--- App.tsx
+++ App.tsx
@@ -1,118 +1,126 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import 'react-native-url-polyfill/auto';
+import '@aws-sdk/util-endpoints';
+import 'react-native-get-random-values';
+import '@ethersproject/shims';
+import {ReadableStream} from 'web-streams-polyfill/ponyfill';
+import 'text-encoding-polyfill';
+globalThis.ReadableStream = ReadableStream;
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import crypto from 'isomorphic-webcrypto';
+
+const prepareCrypto = async () => {
+  console.log('globalThis.crypto', globalThis.crypto);
+  await crypto.ensureSecure();
+  globalThis.crypto = crypto;
+};
+
+import {MMKV} from 'react-native-mmkv';
+
+import {Button, StyleSheet, Text, View} from 'react-native';
+import {SequenceWaaS, defaults} from '@0xsequence/waas';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  GoogleSignin,
+  GoogleOneTapSignIn,
+  statusCodes,
+} from '@react-native-google-signin/google-signin';
+import {ethers} from 'ethers';
+import {useEffect} from 'react';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const projectAccessKey = 'AQAAAAAAAAA5Zk0aenPjVGFFziPaqgAWJX4';
+const waasConfigKey =
+  'eyJwcm9qZWN0SWQiOjU3LCJycGNTZXJ2ZXIiOiJodHRwczovL25leHQtd2Fhcy5zZXF1ZW5jZS5hcHAifQ==';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const storage = new MMKV();
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const localStorage = {
+  get: async (key: string) => {
+    return storage.getString(key) ?? null;
+  },
+  set: async (key: string, value: string) => {
+    if (value === null) {
+      storage.delete(key);
+      return;
+    }
+    storage.set(key, value);
+  },
+};
+
+const sequence = new SequenceWaaS(
+  {
+    network: 'polygon',
+    projectAccessKey: projectAccessKey,
+    waasConfigKey: waasConfigKey,
+  },
+  defaults.TEST,
+  localStorage,
+);
+
+const signIn = async () => {
+  // issue is here, so below this code is not tested because this part is not working
+  const nonce = await sequence.getSessionHash();
+
+  await GoogleSignin.hasPlayServices();
+  const userInfo = await GoogleOneTapSignIn.signIn({
+    webClientId:
+      '277467576806-f8g9k1vu110ssa8lgt4fd2v1gjmc21as.apps.googleusercontent.com',
+    iosClientId:
+      'com.googleusercontent.apps.277467576806-f8g9k1vu110ssa8lgt4fd2v1gjmc21as',
+    nonce: nonce,
+  });
+  console.log(userInfo);
+
+  if (!userInfo.idToken) {
+    console.log('No idToken');
+    return;
+  }
+
+  try {
+    const signInResult = await sequence.signIn(
+      {idToken: userInfo.idToken},
+      'asdasdasd',
+    );
+    console.log(signInResult);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export default function App() {
+  useEffect(() => {
+    prepareCrypto();
+  }, []);
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <Text>Waas auth demo on React Native!</Text>
+      <Button onPress={signIn} title="sign in" />
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
-export default App;
+const DEVICE_EMOJIS = [
+  // 256 emojis for unsigned byte range 0 - 255
+  ...'🐶🐱🐭🐹🐰🦊🐻🐼🐨🐯🦁🐮🐷🐽🐸🐵🙈🙉🙊🐒🐔🐧🐦🐤🐣🐥🦆🦅🦉🦇🐺🐗🐴🦄🐝🐛🦋🐌🐞🐜🦟🦗🕷🕸🦂🐢🐍🦎🦖🦕🐙🦑🦐🦞🦀🐡🐠🐟🐬🐳🐋🦈🐊🐅🐆🦓🦍🦧🐘🦛🦏🐪🐫🦒🦘🐃🐂🐄🐎🐖🐏🐑🦙🐐🦌🐕🐩🦮🐈🐓🦃🦚🦜🦢🦩🕊🐇🦝🦨🦡🦦🦥🐁🐀🐿🦔🐾🐉🐲🌵🎄🌲🌳🌴🌱🌿🍀🎍🎋🍃👣🍂🍁🍄🐚🌾💐🌷🌹🥀🌺🌸🌼🌻🌞🌝🍏🍎🍐🍊🍋🍌🍉🍇🍓🍈🥭🍍🥥🥝🍅🥑🥦🥬🥒🌶🌽🥕🧄🧅🥔🍠🥐🥯🍞🥖🥨🧀🥚🍳🧈🥞🧇🥓🥩🍗🍖🦴🌭🍔🍟🍕🥪🥙🧆🌮🌯🥗🥘🥫🍝🍜🍲🍛🍣🍱🥟🦪🍤🍙🍚🍘🍥🥠🥮🍢🍡🍧🍨🍦🥧🧁🍰🎂🍮🍭🍬🍫🍿🍩🍪🌰🥜👀👂👃👄👅👆👇👈👉👊👋👌👍👎👏👐👑👒👓🎯🎰🎱🎲🎳👾👯👺👻👽🏂🏃🏄',
+];
+
+// Generate a random name for the session, using a single random emoji and 2 random words
+// from the list of words of ethers
+export function randomName() {
+  const wordlistSize = 2048;
+  const words = ethers.wordlists.en;
+
+  const randomEmoji =
+    DEVICE_EMOJIS[Math.floor(Math.random() * DEVICE_EMOJIS.length)];
+  const randomWord1 = words.getWord(Math.floor(Math.random() * wordlistSize));
+  const randomWord2 = words.getWord(Math.floor(Math.random() * wordlistSize));
+
+  return `${randomEmoji} ${randomWord1} ${randomWord2}`;
+}
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    '@babel/plugin-transform-spread',
+    '@babel/plugin-transform-object-rest-spread',
+  ],
 };
